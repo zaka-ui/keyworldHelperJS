5f7a5c9b346236b774d4e947d854536601b1c26f
unknown <leassafrizakaria@gmail.com>
2024-10-31 18:59:47 +0100

    second commit

--- src/app/results/page.js
+++ src/app/results/page.js
@@ -281,7 +281,7 @@ const handleDownloadCSV = () => {
  const analyzeKeywords = async (results) => {
   try {
     setLoader(true)
-    const response = await fetch("http://localhost:3000/api", {
+    const response = await fetch("/api", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
